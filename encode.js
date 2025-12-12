cebdc0b079d4ac161b27eb25baac7ce93f4d43be
Yeasinoncode98 <devoncode98@gmail.com>
2025-12-13 05:03:24 +0600

    Added Admin SDK base64

--- encode.js
+++ encode.js
@@ -2,3 +2,4 @@ const fs = require("fs");
 const key = fs.readFileSync("./assetverse.json", "utf8");
 const base64 = Buffer.from(key).toString("base64");
 console.log(base64);
+// Added to encode and turn the sdk as Base 64"
